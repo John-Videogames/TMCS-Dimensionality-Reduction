126a2f13cec78435128f5158829454ff7d035ded
Karl Michael Ziems <km.z@gmx.de>
2019-05-16 14:22:44 +0100

    Removed remaining line from git conflict solving

--- processing.py
+++ processing.py
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 """Code to process the input matrix via PCA"""
-=======
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-Processing docstring
-"""
->>>>>>> 127386c28d23276ff97d3914333adc7a7c61ec78
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
-<<<<<<< HEAD
 from sklearn.decomposition import PCA
 from preprocessing import *
 
@@ -38,12 +31,3 @@ if __name__ == "__main__":
     PCA_test = PCAResults(input_file) 
     #obtain variance for all PCA components as array
     variances = PCA_test.get_comp_variance()
-    
-=======
-from preprocessing import *
-from sklearn.decomposition import PCA
-
-
-if __name__ == "__main__":
-    pass
->>>>>>> 127386c28d23276ff97d3914333adc7a7c61ec78
