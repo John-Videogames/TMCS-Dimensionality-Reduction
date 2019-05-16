d36d4cb022cbd0748522c656778b694a25d324cf
Matt Bailey <matthew.bailey@chem.ox.ac.uk>
2019-05-16 13:55:33 +0100

    Added some python boilerplate

--- preprocessing.py
+++ preprocessing.py
@@ -1,30 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Pre-processing of xyz files into
+a object wrapping a 3N x M 
+numpy array.
+"""
 
 import numpy as np
 from collections import defaultdict
 
-def cast_num_atoms(num_atoms):
+def cast_positive_int(in_string):
     """
-    Validates a string number of atoms
-    to make sure it is a valid, positive
-    integer, and then returns it.
-    :param num_atoms:
+    Checks that a string is a valid
+    positive integer
+    :param in_string:
     :return:
     """
     try:
-        int(num_atoms)
+        int(in_string)
     except ValueError:
-        raise ValueError(f"Cannot turn {num_atoms} into an integer")
-    if int(num_atoms) != float(num_atoms):
-        raise ValueError(f"{num_atoms} is floating point number, not an int")
+        raise ValueError(f"Cannot turn {in_string} into an integer")
+    if int(in_string) != float(in_string):
+        raise ValueError(f"{in_string} is floating point number, not an int")
 
-    num_atoms = int(num_atoms)
-    if num_atoms == 0:
-        raise ValueError(f"num_atoms cannot be zero.")
+    out_int = int(in_string)
+    if out_int == 0:
+        raise ValueError(f"This integer cannot be zero.")
 
-    if num_atoms < 0:
-        raise ValueError(f"num_atoms cannot be negative.")
+    if out_int < 0:
+        raise ValueError(f"This integer cannot be negative.")
 
-    return num_atoms
+    return out_int
 
 
 class XYZFile:
@@ -54,9 +61,10 @@ class XYZFile:
         atom_labels = []
         for line in lines:
             atom = line.split()[0]
-            seen_atoms[atom] += 1
             atom = f"{atom}{seen_atoms[atom]}"
             atom_labels.extend([f"{atom}_x", f"{atom}_y", f"{atom}_z"])
+            seen_atoms[atom] += 1
+        assert len(atom_labels) == self.num_atoms * 3, "Did not find 3N atomic labels."
         return atom_labels
 
     def parse_one_frame(self, lines):
@@ -70,7 +78,7 @@ class XYZFile:
         frame = []
         for line in lines:
             elements = line.split()[1:]
-            elements = np.asfarray(elements,float)
+            elements = np.asfarray(elements, float)
             frame.extend(elements)
         return np.array(frame)
 
@@ -78,32 +86,27 @@ class XYZFile:
     def parse_xyz_file(self, filename):
         """
         Loads in an .xyz file into
-           a set of 3N x steps vectors
+        a set of 3N atom coordinates x M steps.
         :param filename:
         :return:
         """
 
         assert filename.endswith(".xyz"), "File must be in .xyz format."
-
+        xyz_header_lines = 2
         with open(filename, "r") as infile:
             lines = infile.readlines()
-            self.num_atoms = cast_num_atoms(lines[0])
+            self.num_atoms = cast_positive_int(lines[0])
 
-            xyz_header_lines = 2
-
-            self.num_frames = int(len(lines) / (self.num_atoms + xyz_header_lines))
-            # Make sure we have got a sensible integer, and not a floating
-            # point number.
-            assert self.num_frames == len(lines) / (self.num_atoms + xyz_header_lines)
+            self.num_frames = cast_positive_int(len(lines) / (self.num_atoms + xyz_header_lines))
 
             frames = np.empty([self.num_frames, self.num_atoms * 3])
-            self.atom_labels = self.parse_atom_labels(lines[xyz_header_lines: xyz_header_lines+self.num_atoms])
+            self.atom_labels = self.parse_atom_labels(lines[xyz_header_lines: xyz_header_lines + self.num_atoms])
             for i in range(self.num_frames):
                 start_index = i * (self.num_atoms + xyz_header_lines) + xyz_header_lines
                 end_index = (i + 1) * (self.num_atoms + xyz_header_lines)
 
                 frame_num_atoms_index = start_index - 2
-                frame_num_atoms = cast_num_atoms(lines[frame_num_atoms_index])
+                frame_num_atoms = cast_positive_int(lines[frame_num_atoms_index])
                 assert frame_num_atoms == self.num_atoms, f"""
                                                            num_atoms at line {frame_num_atoms_index }inconsistent.
                                                            Got {frame_num_atoms}, expected {self.num_atoms}.
---
+++ processing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+from preprocessing import *
+from sklearn.decomposition import PCA
+
+
+if __name__ == "__main__":
+    pass
