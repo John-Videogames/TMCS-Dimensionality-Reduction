ee6cbf2b06e87d812a100bf31a70713ce23577fc
Karl Michael Ziems <km.z@gmx.de>
2019-05-16 16:02:39 +0100

    transpose within class

--- processing.py
+++ processing.py
@@ -39,7 +39,7 @@ class PCAResults:
 
     def transform_data(self):
         """Transform data based on fit"""
-        self.transformed_data = self.pca.transform(self.data)
+        self.transformed_data = self.pca.transform(self.data).T
 
     def get_transformed_data(self):
         """Return matrix of transformed data"""
@@ -70,7 +70,7 @@ if __name__ == "__main__":
     #transform data based on PCA fit
     PCA_test_2.transform_data()
     #Example for plotting the transformed data in the coordinate system spanned by PC1 and PC2
-    plt.scatter(PCA_test_2.get_transformed_data().T[0], PCA_test_2.get_transformed_data().T[1])
+    plt.scatter(PCA_test_2.get_transformed_data()[0], PCA_test_2.get_transformed_data()[1])
     plt.show()
 
 
