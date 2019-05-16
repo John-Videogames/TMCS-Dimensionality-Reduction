63a4089aef606fb52d4c4e1bf38d7b8c6901cdab
Matt Bailey <matthew.bailey@chem.ox.ac.uk>
2019-05-16 11:48:09 +0100

    Added an xyz file and started preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,22 @@
+
+
+
+def parse_xyz_file(filename: string):
+    """
+    Loads in an .xyz file into
+    a set of 3N x steps vectors
+    :param filename:
+    :return:
+    """
+
+    with open(filename, "r") as infile:
+        line = infile.readline()
+        try:
+            number_of_atoms = int(line)
+            print(number_of_atoms)
+        except ValueError:
+            print("Bad number of atoms.")
+            return
+
+if __name__ == "__main__":
+    parse_xyz_file("./Resources/")
