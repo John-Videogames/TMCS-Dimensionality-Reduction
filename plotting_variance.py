d133eefdd42fe15c69a2c7e52613c5450435df43
Rhoslyn Roebuck Williams <rhoslynroebuck@gmail.com>
2019-05-17 11:29:55 +0100

    plotting all variances on same graph - working version

--- plotting_variance.py
+++ plotting_variance.py
@@ -12,19 +12,18 @@ from main import *
 
 def cumulative_variance():
 
-
-
     # empty lists for importing data and plotting
     inputxyzs = []
     PCA_test = []
     all_cumulative_variances = []
-    output = range(26)  # THIS IS SUPPOSED TO BE OF LENGTH OF THE PRINCIPLE COMPONENTS??
-
+    component = list(range(26))
 
     # get data from .xyz files
     for file in POSSIBLE_FILES:
-        inputxyzs.append(XYZFile(file))
-        PCA_test = PCAResults(inputxyzs[file])
+
+        xyzresult = XYZFile(file)
+        inputxyzs.append(xyzresult)
+        PCA_test.append(PCAResults(xyzresult,25))
 
     # loop over all input files
     for i in range(len(inputxyzs)):
@@ -38,32 +37,50 @@ def cumulative_variance():
             total_variance += input_values[i]
 
         # Set up lists/values for plotting
-        component = [0]               # x-axis values
         cumulative_variances = [0]    # y-axis values
         cum_variance = 0.
 
         # get data to plot from input values
         for j in range(len(input_values)):
-            component.append(j + 1)
             cum_variance += input_values[j]/ total_variance
             cumulative_variances.append(cum_variance)
 
+        # nested lists for plotting multiple trajectories on one graph
         all_cumulative_variances.append(cumulative_variances)
 
+    # ---------------------------- plotting ------------------------------- #
 
-        # ---------------------------- plotting ------------------------------- #
+    print(component, all_cumulative_variances)
+    print(len(all_cumulative_variances[0]))
 
-        fig = plt.figure(figsize = (5,5))
-        ax = fig.add_subplot(111)
+    fig = plt.figure(figsize = (5,5))
+    ax = fig.add_subplot(111)
 
-        xmin, xmax = -1,25
-        ymin, ymax = 0,1.1
-        ax.plot(component, cumulative_variances, c = 'r' )
-        ax.set_xlabel('Number of components')
-        ax.set_ylabel('Cumulative variance')
-        plt.xlim(xmin, xmax)
-        plt.ylim(ymin, ymax)
-        plt.savefig('cumulative_variances.png')
+    xmin, xmax = -1,25
+    ymin, ymax = 0,1.1
+
+    #plt.xlabel('Number of components')
+    #plt.ylabel('Cumulative variance')
+    #plt.title("A test graph")
+
+    # ax.scatter(component, all_cumulative_variances[0], label='trajectory %s' % i, c='r')
+
+
+    for k in range(len(all_cumulative_variances)):
+       ax.plot(component,all_cumulative_variances[i],label = 'trajectory %s'%k, c = 'r' )
+
+    plt.legend()
+    ax.set_xlabel('Number of components')
+    ax.set_ylabel('Cumulative variance')
+    ax.set_title("A test graph")
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    fig.show()
+    fig.savefig('cumulative_variances.png')
 
 
 
+
+if __name__ == "__main__":
+
+    cumulative_variance()
